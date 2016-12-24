f98998027fb54ab2fbc785a0f732ee788d6feca2
Jen <jenyee1022@yahoo.com>
2016-12-24 10:01:25 -0800

    trying gmail to send email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,12 +95,12 @@ end
 # }
 # ActionMailer::Base.delivery_method = :smtp
 config.action_mailer.delivery_method = :smtp
-# SMTP settings for gmail
 config.action_mailer.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port                 => 587,
-  :user_name            => ENV['gmail_username'],
-  :password             => ENV['gmail_password'],
-  :authentication       => "plain",
-  :enable_starttls_auto => true
-}
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'example.com',
+  user_name:            ENV['gmail_username'],
+  password:             ENV['gmail_password'],
+  authentication:       'plain',
+  enable_starttls_auto: true  }
+
