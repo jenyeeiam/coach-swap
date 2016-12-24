12795c684ed6fdee18ad0e1e8ef41810bf335f9b
Jen <jenyee1022@yahoo.com>
2016-12-24 11:15:26 -0800

    attempted fix to update password in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,7 +94,9 @@ end
 #   :authentication => :plain,
 # }
 #
-# config.action_mailer.delivery_method = :smtp
+config.action_mailer.default_url_options = { :host => 'coach-swap.herokuapp.com' }
+Rails.application.routes.default_url_options[:host] = 'coach-swap.herokuapp.com'
+
 ActionMailer::Base.smtp_settings = {
   address:              'smtp.gmail.com',
   port:                 587,
