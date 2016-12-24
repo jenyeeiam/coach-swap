de91c2bd818f40d2329ce08e70e1bd268129573f
Jen <jenyee1022@yahoo.com>
2016-12-23 20:24:14 -0800

    mailgun settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,13 +85,12 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 end
 
-# config.action_mailer.delivery_method = :smtp
-# # SMTP settings for gmail
-# config.action_mailer.smtp_settings = {
-#   :address              => "smtp.gmail.com",
-#   :port                 => 587,
-#   :user_name            => ENV['gmail_username'],
-#   :password             => ENV['gmail_password'],
-#   :authentication       => "plain",
-#   :enable_starttls_auto => true
-# }
+ActionMailer::Base.smtp_settings = {
+  :port           => ENV['MAILGUN_SMTP_PORT'],
+  :address        => ENV['MAILGUN_SMTP_SERVER'],
+  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+  :domain         => 'coach-swap.herokuapp.com',
+  :authentication => :plain,
+}
+ActionMailer::Base.delivery_method = :smtp
